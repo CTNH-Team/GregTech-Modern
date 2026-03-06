09a86e0fa072f836d49283d54b01101a33965f61
TonyCrane <me@tonycrane.cc>
2026-03-06 21:53:39 +0800

    chore: 是 Core 里 mixin 导致的问题，没必要在矿处图去重输出

--- src/main/java/com/gregtechceu/gtceu/integration/emi/oreprocessing/GTEmiOreProcessing.java
+++ src/main/java/com/gregtechceu/gtceu/integration/emi/oreprocessing/GTEmiOreProcessing.java
@@ -8,10 +8,7 @@ import com.lowdragmc.lowdraglib.emi.ModularEmiRecipe;
 import net.minecraft.resources.ResourceLocation;
 
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
-import dev.emi.emi.api.stack.EmiStack;
 import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 public class GTEmiOreProcessing extends ModularEmiRecipe<GTOreByProductWidget> {
 
@@ -30,11 +27,6 @@ public class GTEmiOreProcessing extends ModularEmiRecipe<GTOreByProductWidget> {
     @Override
     public @Nullable ResourceLocation getId() {
         return material.getResourceLocation();
-    }
-
-    @Override
-    public List<EmiStack> getOutputs() {
-        return super.getOutputs().stream().distinct().toList();
     }
 
     @Override
