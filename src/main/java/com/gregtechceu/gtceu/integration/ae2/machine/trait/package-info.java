1bcb42f8ac2d8447384680b6c80a866ec6e74229
Gate Guardian <gateguardian@qq.com>
2026-03-24 18:13:54 +0800

    remove AE2 multiblock part interfaces

---
+++ src/main/java/com/gregtechceu/gtceu/integration/ae2/machine/trait/package-info.java
@@ -0,0 +1,7 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+package com.gregtechceu.gtceu.integration.ae2.machine.trait;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
