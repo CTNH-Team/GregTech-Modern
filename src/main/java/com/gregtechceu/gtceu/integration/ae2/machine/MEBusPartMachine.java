d5bf0be7966625d74cb5aa7ed5482a7e0f1af111
Gate Guardian <gateguardian@qq.com>
2026-03-24 18:00:38 +0800

    refactor base classes for me part machine

--- src/main/java/com/gregtechceu/gtceu/integration/ae2/machine/MEBusPartMachine.java
+++ src/main/java/com/gregtechceu/gtceu/integration/ae2/machine/MEBusPartMachine.java
@@ -1,96 +1,62 @@
 package com.gregtechceu.gtceu.integration.ae2.machine;
 
-import com.gregtechceu.gtceu.api.GTValues;
+import appeng.api.networking.GridFlags;
+import appeng.api.networking.IGridNode;
+import appeng.api.networking.security.IActionHost;
+import appeng.api.networking.security.IActionSource;
 import com.gregtechceu.gtceu.api.capability.recipe.IO;
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 import com.gregtechceu.gtceu.common.machine.multiblock.part.ItemBusPartMachine;
+import com.gregtechceu.gtceu.config.ConfigHolder;
 import com.gregtechceu.gtceu.integration.ae2.machine.feature.IGridConnectedMachine;
-import com.gregtechceu.gtceu.integration.ae2.machine.trait.GridNodeHolder;
-
-import com.lowdragmc.lowdraglib.syncdata.annotation.DescSynced;
-import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted;
+import com.gregtechceu.gtceu.integration.ae2.machine.trait.GridNodeHost;
 import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder;
-
-import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.Direction;
-
-import appeng.api.networking.*;
-import appeng.api.networking.security.IActionSource;
-import lombok.Getter;
-import lombok.Setter;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.EnumSet;
 
-import javax.annotation.ParametersAreNonnullByDefault;
+public abstract class MEBusPartMachine extends ItemBusPartMachine implements IGridConnectedMachine, IActionHost {
 
-@Getter
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-public abstract class MEBusPartMachine extends ItemBusPartMachine implements IGridConnectedMachine {
+    protected static final ManagedFieldHolder MANAGED_FIELD_HOLDER = new ManagedFieldHolder(
+            MEBusPartMachine.class,
+            ItemBusPartMachine.MANAGED_FIELD_HOLDER
+    );
 
-    protected static final ManagedFieldHolder MANAGED_FIELD_HOLDER = new ManagedFieldHolder(MEBusPartMachine.class,
-            ItemBusPartMachine.MANAGED_FIELD_HOLDER);
+    protected final GridNodeHost nodeHost;
+    protected final IActionSource actionSource = IActionSource.ofMachine(this);
 
-    @Persisted
-    protected final GridNodeHolder nodeHolder;
-
-    @DescSynced
-    @Getter
-    @Setter
-    protected boolean isOnline;
-
-    protected final IActionSource actionSource;
-
-    public MEBusPartMachine(IMachineBlockEntity holder, IO io, Object... args) {
-        super(holder, GTValues.LuV, io, args);
-        this.nodeHolder = createNodeHolder();
-        this.actionSource = IActionSource.ofMachine(nodeHolder.getMainNode()::getNode);
+    // bus LuV
+    // UHV FluidHatchPartMachine.INITIAL_TANK_CAPACITY_1X 16
+    public MEBusPartMachine(IMachineBlockEntity holder, int tier, IO io, Object... args) {
+        super(holder, tier, io, args);
+        this.nodeHost = createNodeHost();
     }
 
-    protected GridNodeHolder createNodeHolder() {
-        return new GridNodeHolder(this);
-    }
-
-    @Override
-    public IManagedGridNode getMainNode() {
-        return nodeHolder.getMainNode();
+    protected GridNodeHost createNodeHost() {
+        GridNodeHost host = new GridNodeHost(this);
+        host.getMainNode()
+                .setFlags(GridFlags.REQUIRE_CHANNEL)
+                .setIdlePowerUsage(ConfigHolder.INSTANCE.compat.ae2.meHatchEnergyUsage)
+                .setExposedOnSides(
+                        hasFrontFacing() ? EnumSet.of(getFrontFacing()) : EnumSet.allOf(Direction.class)
+                );
+        return host;
     }
 
     @Override
-    public void onMainNodeStateChanged(IGridNodeListener.State reason) {
-        IGridConnectedMachine.super.onMainNodeStateChanged(reason);
-        this.updateInventorySubscription();
-    }
-
-    @Override
-    protected void updateInventorySubscription() {
-        if (shouldSubscribe()) {
-            autoIOSubs = subscribeServerTick(autoIOSubs, this::autoIO);
-        } else if (autoIOSubs != null) {
-            autoIOSubs.unsubscribe();
-            autoIOSubs = null;
-        }
-    }
-
-    protected boolean shouldSubscribe() {
-        return isWorkingEnabled() && isOnline();
+    public @Nullable IGridNode getActionableNode() {
+        return nodeHost.getMainNode().getNode();
     }
 
     @Override
     public void onRotated(Direction oldFacing, Direction newFacing) {
         super.onRotated(oldFacing, newFacing);
-        getMainNode().setExposedOnSides(EnumSet.of(newFacing));
+        nodeHost.getMainNode().setExposedOnSides(EnumSet.of(newFacing));
     }
 
     @Override
     public ManagedFieldHolder getFieldHolder() {
         return MANAGED_FIELD_HOLDER;
-    }
-
-    // By returning false here, we don't allow shift-clicking
-    // with a screwdriver to swap the IO.
-    @Override
-    public boolean swapIO() {
-        return false;
     }
 }
